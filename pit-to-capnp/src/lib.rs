a37de810fe7a97f57dc6606b66e7153443f92a96
Graham Kelly <gkgoat6700@gmail.com>
2025-07-16 19:46:35 +0000

    capnp, sdk, and wit

---
+++ pit-to-capnp/src/lib.rs
@@ -0,0 +1,14 @@
+#![no_std]
+
+use core::fmt::{Display, Formatter};
+extern crate alloc;
+pub trait Capnp{
+    fn capnp(&self, f: &mut Formatter<'_>) -> core::fmt::Result;
+}
+#[derive(Clone, Copy)]
+pub struct ViaCapnp<'a>(pub &'a (dyn Capnp + 'a));
+impl Display for ViaCapnp<'_>{
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        self.0.capnp(f)
+    }
+}
---
+++ pit-wit-bridge/src/lib.rs
@@ -0,0 +1,14 @@
+#![no_std]
+extern crate alloc;
+use core::fmt::{Display, Formatter};
+
+pub trait ToWIT{
+    fn to_wit(&self, f: &mut Formatter<'_>) -> core::fmt::Result;
+}
+#[derive(Clone, Copy)]
+pub struct ViaWIT<'a>(pub &'a (dyn ToWIT + 'a));
+impl Display for ViaWIT<'_>{
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        self.0.to_wit(f)
+    }
+}
