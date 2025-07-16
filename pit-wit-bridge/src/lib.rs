cfba6555796417782b4571115be96158dbfa2271
Graham Kelly <gkgoat6700@gmail.com>
2025-07-16 20:43:18 +0000

    sdk

--- pit-wit-bridge/src/lib.rs
+++ pit-wit-bridge/src/lib.rs
@@ -2,12 +2,12 @@
 extern crate alloc;
 use core::fmt::{Display, Formatter};
 
-pub trait ToWIT{
+pub trait ToWIT {
     fn to_wit(&self, f: &mut Formatter<'_>) -> core::fmt::Result;
 }
 #[derive(Clone, Copy)]
 pub struct ViaWIT<'a>(pub &'a (dyn ToWIT + 'a));
-impl Display for ViaWIT<'_>{
+impl Display for ViaWIT<'_> {
     fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
         self.0.to_wit(f)
     }
