895c9995ba3090538a58bf736160f79787b5e11c
Graham Kelly <gkgoat6700@gmail.com>
2025-09-05 16:22:57 +0000

    nullability

--- crates/pit-ts-generic/src/lib.rs
+++ crates/pit-ts-generic/src/lib.rs
@@ -6,10 +6,15 @@ extern crate alloc;
 #[derive(Default, Clone, Debug)]
 #[non_exhaustive]
 pub struct TsOpts {
+    pub r#async: bool,
     // pub rewrites: BTreeMap<[u8; 32], String>,
 }
 impl TsOpts {
     pub fn ty(&self, t: &Arg, this: [u8; 32]) -> String {
+        let m = match self.r#async {
+            true => "A",
+            false => "",
+        };
         match t {
             Arg::I32 => format!("number"),
             Arg::I64 => format!("bigint"),
@@ -20,42 +25,56 @@ impl TsOpts {
                 nullable,
                 take,
                 ann,
-            } => match ty {
+            } => match match ty {
                 pit_core::ResTy::None => format!("any"),
-                pit_core::ResTy::Of(a) => format!(
-                    "P{}",
-                    hex::encode(a)
-                ),
-                pit_core::ResTy::This => format!("P{}", hex::encode(this)),
+                pit_core::ResTy::Of(a) => format!("{m}P{}", hex::encode(a)),
+                pit_core::ResTy::This => format!("{m}P{}", hex::encode(this)),
                 _ => todo!(),
-            },
+            }{
+                ty => if *nullable{
+                    format!("{ty} | undefined")
+                }else{
+                    ty
+                }
+            }
             _ => todo!(),
         }
     }
     pub fn meth(&self, s: &Sig, this: [u8; 32]) -> String {
+        let j = s
+            .rets
+            .iter()
+            .map(|x| self.ty(x, this))
+            .collect::<Vec<_>>()
+            .join(",");
         format!(
-            "({}): [{}]",
+            "({}): [{}]{}",
             s.params
                 .iter()
                 .enumerate()
                 .map(|(a, b)| format!("p{a}: {}", self.ty(b, this)))
                 .collect::<Vec<_>>()
                 .join(","),
-            s.rets
-                .iter()
-                .map(|x| self.ty(x, this))
-                .collect::<Vec<_>>()
-                .join(",")
+            j,
+            if self.r#async {
+                format!("| Promise<[{j}]>")
+            } else {
+                String::default()
+            }
         )
     }
     pub fn interface(&self, i: &Interface) -> String {
         let this = i.rid();
+        let m = match self.r#async {
+            true => "A",
+            false => "",
+        };
         format!(
-            "export type P{} = {{{}}}",
+            "export type {m}P{} = {{{}}}",
             hex::encode(this),
             i.methods
                 .iter()
-                .map(|(a, b)| format!("P{}_{a} {}", hex::encode(this), self.meth(b, this)))
+                .map(|(a, b)| format!("{m}P{}_{a} {}", hex::encode(this), self.meth(b, this)))
                 .collect::<Vec<_>>()
                 .join("")
         )
